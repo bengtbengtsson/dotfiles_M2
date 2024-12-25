bde273ee9c4dc1c661ef3f46d7a5ac0657beb52a
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2024-12-25 14:47:34 +0100

    added neovim config for :terminal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,7 +7,6 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 vim.keymap.set('n', '<C-å>', '<C-]>', { noremap = true, silent = true }) -- CTRL-] does not work
-
 -- highlight when yanking (copying) text
 -- try it with `yap` in normal mode
 -- see `:help vim.highlight.on_yank()`
@@ -18,3 +17,23 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- set no numbers in :terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Open neovim terminal',
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+-- just a shortcut to start :terminal
+-- might not be needed
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+end
+)
