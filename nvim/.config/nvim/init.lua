4ccd241f31402f835a7ac84178e6e4a1ff8a3479
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-01-04 18:59:56 +0100

    disable oil.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,8 @@ require("config.lazy")
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.scrolloff = 7
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
@@ -40,9 +42,9 @@ end
 )
 
 -- oil.nvim
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-require("oil").setup({
-  view_options = {
-    show_hidden = true,
-  },
-})
+--require("oil").setup({
+--view_options = {
+--show_hidden = true,
+--},
+--})
+--vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
