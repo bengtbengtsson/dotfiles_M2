2ad2b6bd4dadfd3563f2bd67762df496ca12c120
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-01-17 19:18:08 +0100

    activate oil.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -42,9 +42,9 @@ end
 )
 
 -- oil.nvim
---require("oil").setup({
---view_options = {
---show_hidden = true,
---},
---})
---vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+require("oil").setup({
+  view_options = {
+    show_hidden = true,
+  },
+})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
