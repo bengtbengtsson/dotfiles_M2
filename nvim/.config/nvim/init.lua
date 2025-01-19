34c9ed39c60d997dcbf735cd71d31f6ba80e87a3
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-01-19 12:10:37 +0100

    major update to neovim init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,69 @@
-require("config.lazy")
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = false
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '80'
+
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
 
 vim.opt.scrolloff = 7
 
+-- execute lua
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- replace CTRL-]
 vim.keymap.set('n', '<C-å>', '<C-]>', { noremap = true, silent = true }) -- CTRL-] does not work
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- highlight when yanking (copying) text
 -- try it with `yap` in normal mode
@@ -20,6 +75,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+require("config.lazy")
 
 -- set no numbers in :terminal
 vim.api.nvim_create_autocmd('TermOpen', {
@@ -40,11 +97,3 @@ vim.keymap.set("n", "<space>st", function()
   vim.api.nvim_win_set_height(0, 5)
 end
 )
-
--- oil.nvim
-require("oil").setup({
-  view_options = {
-    show_hidden = true,
-  },
-})
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- nvim/.config/nvim/lua/config/plugins/oil.lua
+++ nvim/.config/nvim/lua/config/plugins/oil.lua
@@ -3,7 +3,13 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      view_options = {
+        show_hidden = true
+      },
+    },
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    setup = {},
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   }
 }
