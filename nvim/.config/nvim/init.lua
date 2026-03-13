ab9dffb542571d1e8fec5f23b32f1ed1cc29522b
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2026-03-13 08:50:56 +0100

    update nvim.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "  -- optional, for local leader
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.lsp.enable('clangd')
